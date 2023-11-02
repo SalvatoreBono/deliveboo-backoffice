a9b8eab84f57fbb6e2923cc7dc199c22dc242348
PasqualeIsabelli <pasquale0395@gmail.com>
2023-11-02 21:28:49 +0100

    add all js validation

--- public/js/restaurants/restaurants_validation.js
+++ public/js/restaurants/restaurants_validation.js
@@ -1,4 +1,6 @@
+// creo la funzione validate che passo al form per effettuare appunto le varie validazioni dei campi del form
 function validate() {
+  // richiamo nelle seguenti costanti gli id dei vari campi del form, per il campo checkbox utilizzo la classe tramite querySelectorAll
   const email = document.getElementById('inputemail');
   const phone = document.getElementById('inputphone');
   const activity_name = document.getElementById('inputactivity_name');
@@ -6,6 +8,7 @@ function validate() {
   const address = document.getElementById('inputaddress');
   const checkboxes = document.querySelectorAll('.type-checkbox');
 
+  // richiamo gli id dei div assegnati ad ogni campo del form a cui aggiungiamo un messaggio di errore momentaneamente vuoto
   const mailError = document.getElementById('error-email');
   mailError.textContent = '';
   const phoneError = document.getElementById('error-phone');
@@ -19,50 +22,51 @@ function validate() {
   const typeError = document.getElementById('error-type');
   typeError.textContent = '';
 
+  // assegno il valore true alla variabile valid che poi diventerà false all'interno di ogni condizione
   let valid = true;
 
+  // creo le varie condizioni dove al loro interno passo i vari messaggi di errore nel caso in cui il campo viene lasciato vuoto oppure riempito con valori non idonei
   if (!email.value) {
     mailError.textContent = "The email field is required!";
-    email.focus();
     valid = false;
   }
   if (!phone.value) {
     phoneError.textContent = 'The phone field is required!';
-    phone.focus();
     valid = false;
   } else if (phone.value.length !== 10) {
     phoneError.textContent = 'The phone number must be 10 digits long!';
-    phone.focus();
     valid = false;
   }
   if (!activity_name.value) {
     activitynameError.textContent = 'The activity name field is required!';
-    activity_name.focus();
     valid = false;
   }
   if (!img.value) {
     imgError.textContent = 'The image field is required!';
-    img.focus();
     valid = false;
   }
   if (!address.value) {
     addressError.textContent = 'The address field is required!';
-    address.focus();
     valid = false;
   }
   
-  let atLeastOneChecked = false;
+  // creo una variabile checkboxChecked  a cui assegno il valore false
+  let checkboxChecked = false;
+  // creo un ciclo che va ad applicare la condizione ad ogni checkbox
   for (const checkbox of checkboxes) {
+    // creo la condizione che passa il valore true alla variabile checkboxChecked
     if (checkbox.checked) {
-      atLeastOneChecked = true;
+      checkboxChecked = true;
       break;
     }
   }
 
-  if (!atLeastOneChecked) {
+  // assegno il messaggio d'errore al campo visible nel caso in cui non viene selezionata nessuna checkbox
+  if (!checkboxChecked) {
     typeError.textContent = 'Select at least one type!';
     valid = false;
   }
 
+  // ritorno la variabile valid
   return valid;
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,7 +8,7 @@
                     <div class="card-header">{{ __('Register') }}</div>
 
                     <div class="card-body">
-                        <form method="POST" action="{{ route('register') }}">
+                        <form method="POST" action="{{ route('register') }}" onsubmit="return(validate())">
                             @csrf
 
                             <div class="mb-4 row">
@@ -19,7 +19,7 @@
                                     <input id="name" type="text"
                                         class="form-control @error('name') is-invalid @enderror" name="name"
                                         value="{{ old('name') }}" required autocomplete="name" autofocus>
-
+                                    <div id="error-name" class="invalid_feedback"></div>
                                     @error('name')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -35,7 +35,7 @@
                                     <input id="surname" type="text"
                                         class="form-control @error('surname') is-invalid @enderror" name="surname"
                                         value="{{ old('surname') }}" required autocomplete="surname" autofocus>
-
+                                    <div id="error-surname" class="invalid_feedback"></div>
                                     @error('surname')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -52,7 +52,7 @@
                                     <input id="email" type="email"
                                         class="form-control @error('email') is-invalid @enderror" name="email"
                                         value="{{ old('email') }}" required autocomplete="email">
-
+                                    <div id="error-email" class="invalid_feedback"></div>
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -69,7 +69,7 @@
                                     <input id="password" type="password"
                                         class="form-control @error('password') is-invalid @enderror" name="password"
                                         required autocomplete="new-password">
-
+                                    <div id="error-password" class="invalid_feedback"></div>
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -86,6 +86,7 @@
                                     <input id="password-confirm" type="password" class="form-control"
                                         name="password_confirmation" required autocomplete="new-password">
                                 </div>
+                                <div id="error-confirm-password" class="invalid_feedback"></div>
                             </div>
                             <div class="mb-4 row">
                                 <label for="VAT_number"
@@ -95,6 +96,7 @@
                                     <input id="VAT_number" type="text"
                                         class="form-control @error('VAT_number') is-invalid @enderror" name="VAT_number"
                                         required>
+                                    <div id="error-vat-number" class="invalid_feedback"></div>
                                     @error('VAT_number')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -116,4 +118,5 @@
             </div>
         </div>
     </div>
+    <script src="{{ asset('js/users/users_validation.js') }}"></script>
 @endsection
