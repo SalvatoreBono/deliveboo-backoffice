3da9c93a808e1845d7289def08354be86114e122
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-11-14 18:43:58 +0100

    Order page styled

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -2,32 +2,44 @@
 @section('title', 'Orders')
 @section('content')
 
-
-<h1>THIS IS THE ORDER PAGE</h1>
-
-    <div class="container py-5">
-        <h1 class="text-center fw-bold text-uppercase pb-3">I tuoi ordini</h1>
-        <div class="row row-cols-4 g-3">
+<div class="container py-5">
+    <h1 class="text-center fw-bold text-uppercase pb-3 text-danger">I tuoi ordini</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Latest first</th>
+                <th scope="col">Order Date and Time</th>
+                <th scope="col">Name</th>
+                <th scope="col">Surname</th>
+                <th scope="col">Address</th>
+                <th scope="col">Email</th>
+                <th scope="col">Products</th>
+                <th scope="col">Total Price</th>
+            </tr>
+        </thead>
+        <tbody>
             @foreach ($orders as $order)
-                <div class="col">
-                    <div class="card d-flex flex-column justify-content-between h-100">
-                        <div class="card-body h-100">
-                            <h5 class="card-title">{{ $order['customer_name'] }}</h5>
-                            <p class="card-text">{{ $order['customer_surname'] }}</p>
-                        </div>
-                        <div class="card-body">
-                            <p class="card-text">{{ $order['customer_address'] }}</p>
-                            @foreach ($order['products'] as $product)
-                                <p class="card-text">{{ $product['name'] }}</p>
-                                <p class="card-text">{{ $product['pivot']['quantity'] }}</p>
-                            @endforeach
-                        </div>
-                        <p class="card-text">{{ $order['created_at'] }}</p>
-                        </div>
+            <tr>
+                <th scope="row">{{ $loop->index + 1 }}</th>
+                <td>{{ $order['created_at'] }}</td>
+                <td>{{ $order['customer_name'] }}</td>
+                <td>{{ $order['customer_surname'] }}</td>
+                <td>{{ $order['customer_address'] }}</td>
+                <td>{{ $order['customer_email'] }}</td>
+                <td>
+                    @foreach ($order['products'] as $product)
+                    <div>
+                        <span class="card-text">{{ $product['name'] }}</span>
+                        :
+                        <span class="card-text">{{ $product['pivot']['quantity'] }}</span>
                     </div>
-                </div>
+                    @endforeach
+                </td>
+                <td>€. {{ $order['total_price'] }}</td>
+            </tr>
             @endforeach
-        </div>
-    </div>
+        </tbody>
+    </table>
+</div>
 
 @endsection
